da5b47e6fd63cee197116c954f1c5a9f91d47342
Simeon <simeon@twolivesleft.com>
2019-01-03 20:29:42 +1030

    Initial Commit

---
+++ MenuTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MenuTest
+//
+//  Created by Simeon Saint-Saens on 3/1/19.
+//  Copyright © 2019 Two Lives Left. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
