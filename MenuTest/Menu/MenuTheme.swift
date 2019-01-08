08c8c045783d1877b764f04fda48791ccdfb860d
Simeon <simeon@twolivesleft.com>
2019-01-08 21:17:57 +1030

    Updated light menu theme to match included design

--- MenuTest/Menu/MenuTheme.swift
+++ MenuTest/Menu/MenuTheme.swift
@@ -33,16 +33,16 @@ public protocol MenuTheme {
 public struct LightMenuTheme: MenuTheme {
     
     public let font = UIFont.systemFont(ofSize: 16, weight: .medium)
-    public let textColor = UIColor(red: 13/255.0, green: 51/255.0, blue: 48/255.0, alpha: 1.0)
+    public let textColor = UIColor.black
     public let brightTintColor = UIColor.white
     public let darkTintColor = UIColor.black
     public let highlightedTextColor = UIColor.white
-    public let highlightedBackgroundColor = UIColor(red: 55/255.0, green: 188/255.0, blue: 174/255.0, alpha: 1.0)
-    public let backgroundTint = UIColor(red: 25/255.0, green: 149/255.0, blue: 125/255.0, alpha: 0.11)
-    public let gestureBarTint = UIColor(red: 13/255.0, green: 51/255.0, blue: 48/255.0, alpha: 0.17)
+    public let highlightedBackgroundColor = UIColor(red: 0/255.0, green: 122/255.0, blue: 255/255.0, alpha: 1.0)
+    public let backgroundTint = UIColor(white: 1.0, alpha: 0.15)
+    public let gestureBarTint = UIColor(red: 36/255.0, green: 36/255.0, blue: 36/255.0, alpha: 0.17)
     public let blurEffect = UIBlurEffect(style: .light)
-    public let shadowColor = UIColor(red: 103/255.0, green: 165/255.0, blue: 162/255.0, alpha: 1.0)
-    public let shadowOpacity: Float = 1.0
+    public let shadowColor = UIColor.black
+    public let shadowOpacity: Float = 0.3
     public let shadowRadius: CGFloat = 7.0
     public let separatorColor = UIColor(white: 0, alpha: 0.1)
     
